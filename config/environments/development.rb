e52bc211f6cd14aa14dc885832fc88f01abf1586
Consigliere <joey.corleone92@yahoo.com>
2016-02-26 10:23:41 -0500

    Use env vars for config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: '149.210.235.192', port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['OATV_ACTION_MAILER_HOST'] || 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :sendmail
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
