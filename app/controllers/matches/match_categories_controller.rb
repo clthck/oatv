f5e37b886d5d6292ebfacd4eeee10fadd8166373
Consigliere <joey.corleone92@yahoo.com>
2016-02-29 16:24:28 -0500

    Move match_categories logic from controller to model

--- app/controllers/matches/match_categories_controller.rb
+++ app/controllers/matches/match_categories_controller.rb
@@ -11,46 +11,9 @@ class Matches::MatchCategoriesController < ApplicationController
 	# POST
 	# Handles create, update and remove action of DataTables Editor
 	def datatables_editor_cud
-		params_list = params[:data].to_a
-		json_data = {}
-		begin
-			case request.POST[:action]
-			when 'create'
-				attributes = params_list[0][1]
-				category = MatchCategory.create! category_params(attributes)
-			when 'edit'
-				id = params_list[0][0]
-				attributes = params_list[0][1]
-				category = MatchCategory.find(id)
-				category.update category_params(attributes)
-			when 'remove'
-				ids = params_list.collect { |e| e[0] }
-				MatchCategory.where(id: ids).destroy_all
-			end
-			json_data = { data: [category] }
-		rescue ActiveRecord::RecordNotUnique => e
-			json_data = {
-				error: "Ugh!",
-				fieldErrors: [{
-					name: 'name',
-					status: 'Match category name has already been taken.'
-				}]
-			}
-		end
+		json_data = MatchCategory.datatables_editor_cud(request.POST[:action], params[:data].to_a)
 		respond_to do |format|
 			format.json { render json: json_data }
-		end
-	end
-
-	private
-
-	# Get strong parameters for CRUD
-	def category_params(attributes)
-		permissibles = [:name]
-		if attributes.instance_of? Parameters
-			attributes.permit(permissibles)
-		else
-			Parameters.new(attributes).permit(permissibles)
 		end
 	end
 end
--- app/models/match_category.rb
+++ app/models/match_category.rb
@@ -1,3 +1,45 @@
 class MatchCategory < ActiveRecord::Base
 	has_many :matches
+
+	# Handles create, update and remove action of DataTables Editor
+	# and returns json hash
+	def self.datatables_editor_cud(dte_action, data)
+		json_data = {}
+		begin
+			case dte_action
+			when 'create'
+				attributes = data[0][1]
+				category = self.create! category_params(attributes)
+			when 'edit'
+				id = data[0][0]
+				attributes = data[0][1]
+				category = self.find(id)
+				category.update category_params(attributes)
+			when 'remove'
+				ids = data.collect { |e| e[0] }
+				self.where(id: ids).destroy_all
+			end
+			json_data = { data: [category] }
+		rescue ActiveRecord::RecordNotUnique => e
+			json_data = {
+				error: "Ugh!",
+				fieldErrors: [{
+					name: 'name',
+					status: 'Match category name has already been taken.'
+				}]
+			}
+		end
+	end
+
+	private
+
+	# Get strong parameters for DataTables Editor CUD
+	def self.category_params(attributes)
+		permissibles = [:name]
+		if attributes.instance_of? ActionController::Parameters
+			attributes.permit(permissibles)
+		else
+			ActionController::Parameters.new(attributes).permit(permissibles)
+		end
+	end
 end
