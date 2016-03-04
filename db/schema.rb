037cd859479e3a48a7a49332529cd92ee0baeba9
Consigliere <joey.corleone92@yahoo.com>
2016-03-04 04:04:54 -0500

    Add db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303094827) do
+ActiveRecord::Schema.define(version: 20160304064639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clip_categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", index: {name: "index_clip_categories_on_name", unique: true}
   end
 
   create_table "countries", force: :cascade do |t|
